6a2160d90cadb1269357d43a4499509573e51b24
mgilliland23 <mgilliland23@gmail.com>
2019-04-20 15:27:26 -0400

    edited testimonial page

--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core'
+
+
+
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-testimonial',
   templateUrl: './testimonial.component.html',
   styleUrls: ['./testimonial.component.scss']
